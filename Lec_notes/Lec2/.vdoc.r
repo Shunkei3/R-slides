17ef5dad5bf23c1c51dfbc3c1afef2ee7c34cf01
Shunkei <kakim002@umn.edu>
2024-07-19 15:11:59 -0500

    update Lec2 slides

--- Lec_notes/Lec2/.vdoc.r
+++ Lec_notes/Lec2/.vdoc.r
@@ -115,18 +115,9 @@
 #
 #
 #
-#
 #| autorun: true
-library(openintro)
-dim(nycflights)
-#
-#
-#
-#| autorun: true
-# Load nycflights13 package and get flights data`
-library(nycflights13)
-data(flights, package = "nycflights13")
-
+# Load flights data from nycflights13 package.
+flights <- nycflights13::flights
 # Remove rows with missing values (just for convenience)
 flights <- na.omit(flights)
 # Check the class of object
@@ -701,6 +692,8 @@ yield_data_wide
 #
 #
 #
+#
+#| autorun: true
 yield_data_long <- 
   data.table(
     state = c("Kansas", "Nebraska", "Iowa", "Illinois") %>% rep(each = 2),
@@ -730,46 +723,164 @@ dcast(data, LHS ~ RHS , value.var = c("var1", "var2"))
 #
 #
 #
-#| autorun: true
-dcast(yield_data_long, state ~ year , value.var = c("yield", "rainfall"))
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
 #
 #
 #| autorun: true
+yield_data_wide <- dcast(yield_data_long, state ~ year , value.var = c("yield", "rainfall"))
+yield_data_wide
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
+melt(data, id.var = c("id_var1", "id_var2"), measure.vars = c("var1", "var2"))
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# yield columns
+col_yields <- paste0("yield_", 2019:2020)
+# rainfall columns
+col_rainfalls <- paste0("rainfall_", 2019:2020)
 
-# Let's use iris data
-data(iris)
-head(iris)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-flights_mini <- 
-  flights[,head(.SD, 1), by = month] %>% 
-  .[, .(carrier, month, dep_delay, arr_delay)]
+yield_data_long_2 <- 
+  melt(
+    yield_data_wide, 
+    id.vars = "state", 
+    measure.vars = list(col_yields, col_rainfalls), 
+    value.name = c("yield", "rainfall")
+  )
 
-# take a look at the data
-head(flights_mini)
+yield_data_long_2
+
+# If you are familiar with regular expressions, you can do:
+# melt(yield_data_wide, id.vars = "state", measure.vars = patterns("^yield", "^rainfall"), value.name = c("yield", "rainfall"))
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# --- using long-form --- #
+yield_data_long[,.(
+  avg_yield = mean(yield),
+  avg_rainfall = mean(rainfall)
+  ), by = .(state)]
+
+# --- using wide-form --- #
+yield_data_wide[,`:=`(
+  avg_yield = sum(yield_2019 + yield_2020)/2,
+  avg_rainfall = sum(rainfall_2019 + rainfall_2020)/2
+)]
+yield_data_wide[,.(state, avg_yield, avg_rainfall)]
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# === create long_dt (run this code) === #
+yield_data_wide <- dcast(yield_data_long, state ~ year, value.var = c("yield", "rainfall"))
+long_data <- melt(yield_data_wide, id.var = "state")
+#
+#
+#
+#
+#
+# You can write your code here
+#
+#
+#
+#
+#
+#
+# First, I would create two columns: `year` and `type` (to denote year or rainfall) by splitting the `variable` column of long_data.
+# use `tstrsplit()` function to split the variable column by "_"
+long_data[, c("type", "year") := tstrsplit(variable, "_", fixed = TRUE)]
+
+# Now, I don't need the variable column. So, remove it.
+long_data[, variable := NULL]
+
+
+# Finally, I would cast the data to the original long form.
+dcast(long_data, state + year ~ type, value.var = "value")
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
+# Merge data2 to data1 keeping all rows from data1
+merge(x, y, by = "key_column", all.x = TRUE)
+```
+#
+#
+#
+#
+#
+#
 #
 #
 #
@@ -787,10 +898,78 @@ head(flights_mini)
 #
 #
 #| autorun: true
-# Get airlines data from `nycflights13` package
-data(airlines, package = "nycflights13")
-setDT(airlines)
-
+# --- Yield data --- #
+yield_data <- 
+  data.table(
+    state = c("Nebraska", "Iowa", "Minnesota", "Illinois", "Kansas"),
+    yield = runif(n = 5, min = 180, max = 280)
+  )
+```
+#
+#
+#
+#
+#
+#| autorun: true
+# --- Weather data --- #
+weather_data <-
+  data.table(
+    state = c("Iowa", "Minnesota", "Nebraska", "Illinois", "Wisconsin"),
+    total_precip = runif(5, min = 10, max = 20)
+  )
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+yield_weather_data <- merge(yield_data, weather_data, by = "state", all.x = TRUE)
+# check the merged data
+yield_weather_data
+#
+#
+#
+#
+#
+#
+weather_yield_data <- merge(weather_data, yield_data , by = "state", all.x = TRUE)
+# check the merged data
+weather_yield_data
+#
+#
+#
+#
+#
+#
+#
+#
+weather_yield_data_all <- merge(weather_data, yield_data , by = "state", all = TRUE)
+# check the merged data
+weather_yield_data_all
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| autorun: true
+airlines <- nycflights13::airlines
 head(airlines)
 #
 #
@@ -799,10 +978,264 @@ head(airlines)
 #
 #
 #
-flights_merged <- airlines[flights, on = "carrier"]
+# You can write your code here
+#
+#
+#
+#
+#
+#
+#
+#
+#
+flights_merged <- merge(flights, airlines, by = "carrier", all.x = TRUE)
 
-# Check whether `name` column is added to `flights` data
-head(flights_merged)
+# using dplyr, this is equivalent to
+# flights_merged <- left_join(flights, airlines, by = "carrier")
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| autorun: true
+yield_data <- 
+  data.table(
+    state = rep(c("Iowa", "Minnesota", "Illinois", "Kansas", "Wisconsin"), each = 2),
+    year = rep(2010:2011, times = 5),
+    yield = runif(n = 10, min = 180, max = 280)
+  )
+
+weather_data <- 
+  data.table(
+    state = rep(c("Iowa", "Minnesota", "Illinois", "Kansas", "Ohio"), each = 4),
+    year = rep(2010:2013, times = 5),
+    total_precip = runif(20, min = 10, max = 20)
+  )
+#
+#
+#
+#
+#
+#
+#
+# you can write your code here
+#
+#
+#
+#
+#
+#
+#
+yield_weather_data <- merge(yield_data, weather_data, by = c("state", "year"), all.x = TRUE)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+flights <- nycflights13::flights # Load flights data from nycflights13
+flights_dt <- as.data.table(flights) # change the data to data.table class 
+flights_mini <- flights_dt[,.(year, month, origin, dest, carrier, air_time, dep_delay, arr_delay)] # select some columns
+flights_mini <- na.omit(flights_mini) # remove rows with missing values
+#
+#
+#
+#
+#
+#
+#
+#
+#
+flights_mini <- na.omit(data.table(nycflights13::flights)[,.(year, month, origin, dest, carrier, air_time, dep_delay, arr_delay)])
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
+fun1(input1) 
+```
+#
+#
+#| eval: false
+input1 %>% fun1()
+#
+#
+#
+#
+#
+#| eval: false
+output1 <- fun1(input1)
+output2 <- fun2(output1)
+```
+#
+#
+#| eval: false
+output2 <- fun1(input1) %>% fun2()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
+fun(x1, x2, x3)
+#
+#
+#
+#
+#| eval: false
+z %>% fun(x2, x3)
+#
+#
+#
+#
+#
+#| eval: false
+fun(z, x2, x3)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Let's use this function (Don't worry about )
+print_three_words <- function(x, y, z) paste(c(x, y, z),collapse = " ")
+# For example, this function print three words with space between them
+print_three_words(x="I", y="love", z="R")
+
+# pass the input to the first argument
+"I" %>% print_three_words(x=., y="love", z="R")
+
+# pass the input to the second argument
+"love" %>% print_three_words(x="I", y=., z="R")
+
+# pass the input to the third argument
+"R" %>% print_three_words(x="I", y="love", z=.)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| auto-run: true
+flights <- nycflights13::flights #load flights datas
+flights_dt <- as.data.table(flights)  # to data.table class
+flights_mini <- flights_dt[,.(year, month, origin, dest, carrier, air_time, dep_delay, arr_delay)] #select some columns
+flights_mini <- na.omit(flights_mini) #remove rows with missing values
+#
+#
+#
+#
+#| auto-run: true
+library(dplyr)
+
+flights_mini <- 
+  nycflights13::flights %>% #load flights data
+  as.data.table(.) %>% # to data.table class
+  .[,.(year, month, origin, dest, carrier, air_time, dep_delay, arr_delay)] %>% #select some columns
+  na.omit(.) #remove rows with missing values
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #
 #
 #
@@ -883,103 +1316,6 @@ flights[,season := fcase(
 #
 #
 #
-#| autorun: true
-library(dplyr)
-
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-flights[dep_delay == max(dep_delay), .(carrier)]
-#
-#
-#
-#
-#
-msp_feb_flights <- flights[dest=="MSP" & month==2L]
-nrow(msp_feb_flights)
-#
-#
-#
-#
-#
-#
-#
-msp_feb_flights[,.(
-  median = median(arr_delay),
-  IQR = quantile(arr_delay, 0.75) - quantile(arr_delay, 0.25),
-  n_flights = .N
-  ), by = carrier]
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# first three columns
-select_cols <- c("year", "month", "day")
-flights[ , select_cols, with = FALSE]
-flights[ , ..select_cols]
-
-# flights[,year:day]
-#
-#
-#
-#
-#
-flights_df <- as.data.frame(flights)
-  with(
-    flights, 
-     mean(arr_delay + dep_delay)
-  )
-
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
 #
 #
 #
@@ -1028,13 +1364,101 @@ flights_mini[, print(.SD), by = month]
 #
 #
 #
-flights[carrier == "AA", .(.N), by = .(origin, month)] %>% head()
 #
 #
 #
 #
 #
-
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #
 #
 #
